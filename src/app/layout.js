d5fea688009f7d0e27b83b971c68b6532a1733af
Zoran Nikolov <zoranstrumica7@gmail.com>
2024-08-21 10:12:05 +0200

    add header section

--- src/app/layout.js
+++ src/app/layout.js
@@ -1,17 +1,21 @@
 import { Inter } from "next/font/google";
 import "./globals.css";
+import Header from "@/components/Header";
 
 const inter = Inter({ subsets: ["latin"] });
 
 export const metadata = {
-  title: "IMDb Clone",
-  description: "This is a movie database Clone",
+  title: "IMDb clone",
+  description: "This is a movie database clone",
 };
 
 export default function RootLayout({ children }) {
   return (
     <html lang="en">
-      <body className={inter.className}>{children}</body>
+      <body className={inter.className}>
+        <Header />
+        {children}
+      </body>
     </html>
   );
 }
